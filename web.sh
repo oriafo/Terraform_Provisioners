4048863f02ae36f0360ef05cdf27fa8ab5f027d2
oriafo <abrahamibharunujele@gmail.com>
2024-09-30 08:37:15 +0100

    modified we.sh

--- web.sh
+++ web.sh
@@ -9,6 +9,7 @@ sudo systemctl start nginx
 sudo systemctl enable nginx
 sudo systemctl status nginx
 sudo wget https://www.tooplate.com/zip-templates/2137_barista_cafe.zip
+#sudo wget https://www.tooplate.com/zip-templates/2137_barista_cafe.zip
 sudo unzip 2137_barista_cafe.zip -d /var/www/html
 sudo cp -r /var/www/html/2137_barista_cafe/* /var/www/html
 sudo nginx -s reload
