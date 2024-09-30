da0e4660ca86f2cca46b3ac59ce8f33a92de4163
oriafo <abrahamibharunujele@gmail.com>
2024-09-30 07:38:32 +0100

    installtion Script

--- web.sh
+++ web.sh
@@ -1,13 +1,16 @@
 #!/bin/bash -xe
+set -e
 sudo apt update
-apt install wget unzip -y
+sudo apt install wget unzip -y
 sudo apt install nginx -y
-sudo ufw allow 'Nginx HTTP'
-sudo ufw status
-systemctl enable nginx
-systemctl status nginx
-wget https://www.tooplate.com/zip-templates/2137_barista_cafe.zip
-unzip 2137_barista_cafe.zip -d /var/www/html
-cp -r /var/www/html/2137_barista_cafe/* /var/www/html
-nginx -s reload
-systemctl restart nginx
+# sudo ufw allow 'Nginx HTTP'
+# sudo ufw status
+sudo systemctl start nginx
+sudo systemctl enable nginx
+sudo systemctl status nginx
+sudo wget https://www.tooplate.com/zip-templates/2137_barista_cafe.zip
+sudo unzip 2137_barista_cafe.zip -d /var/www/html
+sudo cp -r /var/www/html/2137_barista_cafe/* /var/www/html
+sudo nginx -s reload
+sudo systemctl restart nginx
+
